63abbc864d1ae8f0b802f50182b1099a64a87abf
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-14 09:55:13 +0200

    Initialite the websocket connection

--- chat/websocket.go
+++ chat/websocket.go
@@ -3,6 +3,8 @@ package chat
 import (
 	"crypto/rand"
 	"fmt"
+
+	"github.com/gorilla/websocket"
 )
 
 // A Client will connect to the Olivia's server using the Token the Information and the Locale
@@ -10,18 +12,36 @@ type Client struct {
 	Information map[string]interface{}
 	Locale      string
 	Token       string
+	Connection  *websocket.Conn
 	Channel     chan string
 }
 
 // NewClient creates a new Client by generating a random token, and setting english as the
 // default langauge.
 // The host is also required with a boolean, if the SSL certificate is required.
-func NewClient(host string, ssl bool) Client {
-	return Client{
-		Locale:  "en",
-		Token:   generateToken(),
-		Channel: make(chan string),
+func NewClient(host string, ssl bool) (Client, error) {
+	// Initialite the scheme and the url
+	scheme := "ws"
+	if ssl {
+		scheme += "s"
 	}
+
+	url := fmt.Sprintf("%s://%s", scheme, host)
+
+	// Create the websocket connection
+	connection, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return Client{}, err
+	}
+
+	defer connection.Close()
+
+	return Client{
+		Locale:     "en",
+		Token:      generateToken(),
+		Connection: connection,
+		Channel:    make(chan string),
+	}, nil
 }
 
 // generateToken returns a random token of 50 characters
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/olivia-ai/olivia-kit-go
 
 go 1.14
+
+require github.com/gorilla/websocket v1.4.2
