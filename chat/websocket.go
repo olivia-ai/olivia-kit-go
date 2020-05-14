c081e41721ef02b6483db6b4fcab62e438a27a35
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-14 10:29:00 +0200

    Create the Handshake method on Client

--- chat/websocket.go
+++ chat/websocket.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 
 	"github.com/gorilla/websocket"
@@ -9,17 +10,27 @@ import (
 
 // A Client will connect to the Olivia's server using the Token the Information and the Locale
 type Client struct {
-	Information map[string]interface{}
+	Information *map[string]interface{}
 	Locale      string
 	Token       string
 	Connection  *websocket.Conn
 	Channel     chan string
 }
 
+// RequestMessage is the structure that uses entry connections to chat with the websocket
+type RequestMessage struct {
+	Type        int                    `json:"type"` // 0 for handshakes and 1 for messages
+	Content     string                 `json:"content"`
+	Token       string                 `json:"user_token"`
+	Information map[string]interface{} `json:"information"`
+	Locale      string                 `json:"locale"`
+}
+
 // NewClient creates a new Client by generating a random token, and setting english as the
 // default langauge.
+// You need to give a pointer of the information map of your client.
 // The host is also required with a boolean, if the SSL certificate is required.
-func NewClient(host string, ssl bool) (Client, error) {
+func NewClient(host string, ssl bool, information *map[string]interface{}) (Client, error) {
 	// Initialite the scheme and the url
 	scheme := "ws"
 	if ssl {
@@ -37,11 +48,32 @@ func NewClient(host string, ssl bool) (Client, error) {
 	defer connection.Close()
 
 	return Client{
-		Locale:     "en",
-		Token:      generateToken(),
-		Connection: connection,
-		Channel:    make(chan string),
+		Information: information,
+		Locale:      "en",
+		Token:       generateToken(),
+		Connection:  connection,
+		Channel:     make(chan string),
 	}, nil
+}
+
+// Handshake performs the handshake request to the websocket
+func (client *Client) Handshake() error {
+	// Marshal the RequestMessage with type 0
+	bytes, err := json.Marshal(RequestMessage{
+		Type:        0,
+		Content:     "",
+		Information: *client.Information,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Write the message to the websocket
+	if err = client.Connection.WriteMessage(websocket.TextMessage, bytes); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // generateToken returns a random token of 50 characters
